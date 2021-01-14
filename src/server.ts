bb186f46fa808bad98e5b9babbf73b8ff5c3f9f1
chamith <chamithadl24@gmail.com>
2021-01-14 20:32:19 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,8 @@
 import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
-
-import logger from "./logger";
-
 const app = express();
 
-/**
- * Setup listener port
- */
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  logger.info(`Running Node.js version ${process.version}`);
-  logger.info(`App environment: ${process.env.NODE_ENV}`);
-  logger.info(`App is running on port ${PORT}`);
-});
+app.get("/", (req, res) => res.json(JSON.stringify({ ok: 1 })).end());
 
-export default app;
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Example app listening on port 3000!")
+);
