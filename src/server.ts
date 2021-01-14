a62d60e82e1a80b4b534c21f4f75d26b9eaaa5ef
chamith <chamithadl24@gmail.com>
2021-01-14 20:36:39 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => res.json(JSON.stringify({ ok: 1 })).end());
+app.get("/", (req, res) => res.send("Productizecrm Backend"));
 
-app.listen(process.env.PORT || 5000, () =>
+app.listen(process.env.PORT || 3000, () =>
   console.log("Example app listening on port 3000!")
 );
