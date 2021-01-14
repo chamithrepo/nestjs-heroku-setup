25a0f0c6ec4687fc86dde4b69ee99082055979a7
chamith <chamithadl24@gmail.com>
2021-01-14 20:00:05 +0530

    dfg

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,6 @@ const app = express();
 
 app.get("/", (req, res) => res.json(JSON.stringify({ ok: 1 })).end());
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Example app listening on port 3000!")
+);
